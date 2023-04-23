75ee317acff6330be163ad266e51c13025bcf95b
kapitanchuk <kapitan1105@gmail.com>
2023-04-23 18:58:03 +0200

    added sayName function (trying meld utility for merging)

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ function doSth(){
     console.log('Say my name, you are god damn right')
 }
 
+function sayName(){
+    return {name:"Validated",age:"profitable"}
+}
+
 const doSth2 = () =>{
     setInterval(doSth,1000);
 } 
